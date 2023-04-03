2076e8d802ba87c8e4473f87f968151f3cb06fb2
abertnamanya <abertnamanya@gmail.com>
2023-04-03 21:58:46 +0300

    Ozone OpenElis Global Intergration

--- start-demo.sh
+++ start-demo.sh
@@ -2,7 +2,7 @@
 export OZONE_DIR=$PWD/ozone && \
 mkdir -p $OZONE_DIR
 # Downloads the project
-export VERSION=1.0.0-alpha.1 && \
+export VERSION=1.0.0-SNAPSHOT && \
 ./mvnw org.apache.maven.plugins:maven-dependency-plugin:3.2.0:get -DremoteRepositories=https://nexus.mekomsolutions.net/repository/maven-public -Dartifact=com.ozonehis:ozone-distro:$VERSION:zip -Dtransitive=false --legacy-local-repository && \
 ./mvnw org.apache.maven.plugins:maven-dependency-plugin:3.2.0:unpack -Dproject.basedir=$OZONE_DIR -Dartifact=com.ozonehis:ozone-distro:$VERSION:zip -DoutputDirectory=$OZONE_DIR/ozone-distro-$VERSION
 # Exports required environment variables
@@ -13,17 +13,17 @@ export OPENMRS_CORE_PATH=$DISTRO_PATH/openmrs_core
 export OPENMRS_MODULES_PATH=$DISTRO_PATH/openmrs_modules
 export EIP_PATH=$DISTRO_PATH/eip_config
 export SPA_PATH=$DISTRO_PATH/spa
-export SENAITE_CONFIG_PATH=$DISTRO_PATH/senaite_config
 export ODOO_EXTRA_ADDONS=$DISTRO_PATH/odoo_config/addons
 export ODOO_CONFIG_PATH=$DISTRO_PATH/odoo_config/odoo_csv
 export ODOO_INITIALIZER_CONFIG_FILE_PATH=$DISTRO_PATH/odoo_config/config/initializer_config.json
 export ODOO_CONFIG_FILE_PATH=$DISTRO_PATH/odoo_config/config/odoo.conf
-export O3_FRONTEND_TAG=3.0.0-beta.2
-
 DOCKER_SERVER_VERSION=$(docker version -f "{{.Server.Version}}")
 DOCKER_SERVER_VERSION_MAJOR=$(echo "$DOCKER_SERVER_VERSION"| cut -d'.' -f 1)
 DOCKER_SERVER_VERSION_MINOR=$(echo "$DOCKER_SERVER_VERSION"| cut -d'.' -f 2)
 DOCKER_SERVER_VERSION_BUILD=$(echo "$DOCKER_SERVER_VERSION"| cut -d'.' -f 3)
+
+cp ./openmrs/custom_modules/event-2.10.0.omod $DISTRO_PATH/openmrs_modules
+cp ./openmrs/custom_modules/labonfhir-1.2.1.omod $DISTRO_PATH/openmrs_modules
 
 if [ "${DOCKER_SERVER_VERSION_MAJOR}" -ge 20 ] && \
    [ "${DOCKER_SERVER_VERSION_MINOR}" -ge 10 ]  && \
